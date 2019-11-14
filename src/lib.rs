143bef1f1774edb1ec24a9e1273a0fbe6a46e998
naia-rs <naia.rust.crate@gmail.com>
2019-11-14 10:46:55 -0700

    idk if this is a good idea..

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,7 @@ pub mod shared;
 #[cfg(feature = "Client")]
 pub mod client;
 
-#[cfg(feature = "Client")]
-pub fn main() {
-    client::main();
-}
-
 #[cfg(feature = "Server")]
 pub mod server;
 
-#[cfg(feature = "Server")]
-pub fn main() {
-    server::main();
-}
 
--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,45 @@
 
 
-use crate::gaia_data_transport::server::{ Server, ServerImpl, instance, set_instance};
+//use crate::gaia_data_transport::server::{ Server, ServerImpl, instance, set_instance};
+//
+//pub fn main() {
+//    unsafe { set_instance(ServerImpl::new().unwrap()) };
+//}
+//
+//pub(crate) fn server() -> &'static mut ServerImpl {
+//    unsafe { instance() }
+//}
 
-pub fn main() {
-    unsafe { set_instance(ServerImpl::new().unwrap()) };
+pub struct Server {
+
 }
 
-pub(crate) fn server() -> &'static mut ServerImpl {
-    unsafe { instance() }
+impl Server {
+    pub fn new() -> Server { //args should take a shared config, and a port
+        Server {}
+    }
+
+    pub fn onConnect(&mut self, func: fn()) { //function should have client, clientData, and callback?
+
+    }
+
+    pub fn onDisconnect(&mut self, func: fn()) { //function should have client
+
+    }
+
+    pub fn addEntity(&mut self) {
+
+    }
+
+    pub fn removeEntity(&mut self) {
+
+    }
+
+    pub fn getNextMessage(&mut self) {
+
+    }
+
+    pub fn update(&mut self) {
+
+    }
 }
