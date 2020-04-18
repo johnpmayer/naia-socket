f71d23686607cf81ddca9105ccbca57588a65307
naia-rs <naia.rust.crate@gmail.com>
2020-04-18 16:36:12 -0600

    committing clone of 'gaia'

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+
+mod internal_shared;
+
+pub mod shared;
+
+#[cfg(any(feature = "WebrtcClient", feature = "UdpClient"))]
+mod client;
+
+#[cfg(any(feature = "WebrtcClient", feature = "UdpClient"))]
+pub use client::Client as Client;
+
+#[cfg(any(feature = "WebrtcServer", feature = "UdpServer"))]
+mod server;
+
+#[cfg(any(feature = "WebrtcServer", feature = "UdpServer"))]
+pub use server::Server as Server;
---
+++ src/server/socket/webrtc_server_socket/mod.rs
@@ -0,0 +1,54 @@
+use crate::server::socket::ServerSocket;
+use super::client_socket::ClientSocket;
+
+pub struct WebrtcServerSocket {
+    connect_function: Option<Box<dyn Fn(&ClientSocket)>>,
+    receive_function: Option<Box<dyn Fn(&ClientSocket, &str)>>,
+    disconnect_function: Option<Box<dyn Fn(&ClientSocket)>>,
+}
+
+impl ServerSocket for WebrtcServerSocket {
+    fn new() -> WebrtcServerSocket {
+        println!("Hello WebrtcServerSocket!");
+
+        let new_server_socket = WebrtcServerSocket {
+            connect_function: None,
+            receive_function: None,
+            disconnect_function: None
+        };
+
+        new_server_socket
+    }
+
+    fn listen(&self, address: &str) {
+
+    }
+
+    fn on_connection(&mut self, func: impl Fn(&ClientSocket) + 'static) {
+        self.connect_function = Some(Box::new(func));
+    }
+
+    fn on_receive(&mut self, func: impl Fn(&ClientSocket, &str) + 'static) {
+        self.receive_function = Some(Box::new(func));
+    }
+
+    fn on_error(&mut self, func: impl Fn<(&ClientSocket, &str), Output=_>) {
+        unimplemented!()
+    }
+
+    fn on_disconnection(&mut self, func: impl Fn(&ClientSocket) + 'static) {
+        self.disconnect_function = Some(Box::new(func));
+    }
+}
+//
+//fn get_available_port(ip: &str) -> Option<u16> {
+//    (8000..9000)
+//        .find(|port| port_is_available(ip, *port))
+//}
+//
+//fn port_is_available(ip: &str, port: u16) -> bool {
+//    match TcpListener::bind((ip, port)) {
+//        Ok(_) => true,
+//        Err(_) => false,
+//    }
+//}
