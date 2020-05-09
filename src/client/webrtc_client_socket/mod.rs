fe589c9977b9edbf538378c2717da7f6faec8226
naia-rs <naia.rust.crate@gmail.com>
2020-05-09 13:54:12 -0600

    minor

--- src/client/webrtc_client_socket/mod.rs
+++ src/client/webrtc_client_socket/mod.rs
@@ -10,40 +10,4 @@ impl ClientSocket for WebrtcClientSocket {
         println!("Hello WebrtcClientSocket!");
         Ok(WebrtcClientSocket {})
     }
-
-    fn on_connection(&self, func: fn()) {
-
-    }
-
-    fn on_disconnection(&self, func: fn()) {
-
-    }
-
-    fn on_receive(&self, func: fn(&str)) {
-
-    }
-
-    fn on_error(&self, func: fn(&str)) {
-
-    }
-
-    fn connect<S>(&self, address: &str) {
-
-    }
-
-    fn disconnect(&self) {
-
-    }
-
-    fn send<S>(&self, msg: &str) {
-
-    }
-
-    fn update(&mut self) {
-        unimplemented!()
-    }
-
-    fn close(&self) {
-        unimplemented!()
-    }
 }
