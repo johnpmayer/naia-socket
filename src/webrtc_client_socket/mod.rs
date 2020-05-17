51da4289f1f67055e1c46b51a154cea1fc30294f
naia-rs <naia.rust.crate@gmail.com>
2020-05-16 21:52:06 -0600

    webrtc example is working now .. kind of cheating under the hood though

--- src/webrtc_client_socket/mod.rs
+++ src/webrtc_client_socket/mod.rs
@@ -1,7 +1,9 @@
 
 use std::net::{SocketAddr, Ipv4Addr, IpAddr};
 
-use crate::client::{ClientSocket};
+extern crate webrtc_client_dep;
+
+use crate::{ClientSocket};
 use super::socket_event::SocketEvent;
 use super::message_sender::MessageSender;
 //use crate::internal_shared::{CLIENT_HANDSHAKE_MESSAGE, SERVER_HANDSHAKE_MESSAGE};
@@ -14,6 +16,8 @@ impl ClientSocket for WebrtcClientSocket {
 
     fn bind(address: &str) -> WebrtcClientSocket {
         info!("Hello WebrtcClientSocket!");
+
+        webrtc_client_dep::setup_webrtc_stuff();
 
         WebrtcClientSocket {
             //address: address.parse().unwrap()
