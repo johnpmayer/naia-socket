7b3b7563a13ad5de8f5e9300993bb81600b98a46
naia-rs <naia.rust.crate@gmail.com>
2020-05-17 10:07:14 -0600

    moved off of webrtc-client-dep!

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 #[macro_use]
 extern crate log;
 
+#[cfg(target_arch = "wasm32")]
+#[macro_use]
+extern crate serde_derive;
+
 #[cfg(not(target_arch = "wasm32"))]
 mod udp_client_socket;
 #[cfg(target_arch = "wasm32")]
