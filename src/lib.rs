4d8dc8969ed03078503bc6bf7fffdd8671901b7f
naia-rs <naia.rust.crate@gmail.com>
2020-05-19 10:44:21 -0600

    removing main_common

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,28 @@ extern crate log;
 use wasm_bindgen::prelude::*;
 
 #[cfg(target_arch = "wasm32")]
-pub mod main_common;
-
-#[cfg(target_arch = "wasm32")]
 mod app;
 
 #[cfg(target_arch = "wasm32")]
 mod app_wasm;
 
 #[cfg(target_arch = "wasm32")]
+use crate::app_wasm::App;
+
+const SERVER_IP_ADDRESS: &str = "192.168.1.6";
+const SERVER_PORT: &str = "3179";
+
+#[cfg(target_arch = "wasm32")]
 #[wasm_bindgen(start)]
 pub fn main_js() {
-    main_common::main_common();
+    // Uncomment the line below to enable logging. You don't need it if something else (e.g. quicksilver) is logging for you
+    web_logger::custom_init(web_logger::Config { level: log::Level::Info });
+
+    info!("Client Example Started");
+
+    let server_socket_address = SERVER_IP_ADDRESS.to_owned() + ":" + SERVER_PORT;
+
+    let mut app = App::new(&server_socket_address);
+
+    app.start_loop();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 
+#[cfg(not(target_arch = "wasm32"))]
 #[macro_use]
 extern crate log;
 
 #[cfg(not(target_arch = "wasm32"))]
-mod main_common;
+use gaia_socket_shared::{find_my_ip_address};
 
 #[cfg(not(target_arch = "wasm32"))]
 mod app;
@@ -12,6 +13,20 @@ mod app;
 mod app_native;
 
 #[cfg(not(target_arch = "wasm32"))]
+use crate::app_native::App;
+
+const SERVER_PORT: &str = "3179";
+
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    main_common::main_common();
+    // Uncomment the line below to enable logging. You don't need it if something else (e.g. quicksilver) is logging for you
+    simple_logger::init_with_level(log::Level::Info).expect("A logger was already initialized");
+
+    info!("Client Example Started");
+
+    let server_socket_address = find_my_ip_address::get() + ":" + SERVER_PORT;
+
+    let mut app = App::new(&server_socket_address);
+
+    app.start_loop();
 }
