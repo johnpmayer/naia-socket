70aebd2f1f842ac9488e1edf88025d68f1bad01f
naia-rs <naia.rust.crate@gmail.com>
2020-05-21 16:17:44 -0600

    yeah, hopefully GaiaServerSocketErrors will be useful later

--- src/error.rs
+++ src/error.rs
@@ -3,16 +3,12 @@ use std::fmt;
 
 #[derive(Debug)]
 pub enum GaiaServerSocketError {
-
-    Something,
-
     Wrapped(Box<dyn Error>)
 }
 
 impl fmt::Display for GaiaServerSocketError {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         match self {
-            GaiaServerSocketError::Something => write!(f, "something"),
             GaiaServerSocketError::Wrapped(boxed_err) => fmt::Display::fmt(boxed_err.as_ref(), f),
         }
     }
