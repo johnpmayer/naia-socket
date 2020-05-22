3707551d63813f0d533577edf6ca168db7a26025
naia-rs <naia.rust.crate@gmail.com>
2020-05-22 12:11:38 -0600

    added config

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,7 @@ pub mod find_available_port;
 pub mod find_my_ip_address;
 
 mod constants;
-pub use constants::{SERVER_HANDSHAKE_MESSAGE, CLIENT_HANDSHAKE_MESSAGE};
+pub use constants::{SERVER_HANDSHAKE_MESSAGE, CLIENT_HANDSHAKE_MESSAGE, DEFAULT_MTU};
+
+mod config;
+pub use config::Config;
