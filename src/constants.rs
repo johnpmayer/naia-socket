7d8ee9bf2274399f7ce296d645cb60de1fb6be43
naia-rs <naia.rust.crate@gmail.com>
2020-05-23 16:31:21 -0600

    some utils to use for headers

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,23 @@
-pub const CLIENT_HANDSHAKE_MESSAGE: &str = "client-handshake-request";
-pub const SERVER_HANDSHAKE_MESSAGE: &str = "server-handshake-response";
+
+#[repr(u8)]
+pub enum MessageHeader {
+    ServerHandshake = 0,
+    ClientHandshake = 1,
+    Heartbeat = 2,
+    Data = 3,
+    Unknown = 255
+}
+
+impl From<u8> for MessageHeader {
+    fn from(orig: u8) -> Self {
+        match orig {
+            0 => return MessageHeader::ServerHandshake,
+            1 => return MessageHeader::ClientHandshake,
+            2 => return MessageHeader::Heartbeat,
+            3 => return MessageHeader::Data,
+            _ => return MessageHeader::Unknown,
+        };
+    }
+}
+
 pub const DEFAULT_MTU: u16 = 1452;
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,10 @@ pub mod find_available_port;
 pub mod find_my_ip_address;
 
 mod constants;
-pub use constants::{SERVER_HANDSHAKE_MESSAGE, CLIENT_HANDSHAKE_MESSAGE, DEFAULT_MTU};
+pub use constants::{MessageHeader, DEFAULT_MTU};
 
 mod config;
 pub use config::Config;
+
+mod utils;
+pub use utils::{StringUtils};
