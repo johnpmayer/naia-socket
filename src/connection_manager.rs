b24a7c422a716eb2095a8b2d4732437aa7b108c5
naia-rs <naia.rust.crate@gmail.com>
2020-05-26 12:16:58 -0600

    refactored Timer out of ConnectionManager

--- src/connection_manager.rs
+++ src/connection_manager.rs
@@ -1,87 +1,33 @@
 extern crate log;
 
-cfg_if! {
-    if #[cfg(target_arch = "wasm32")] {
-        /// WebRTC ///
-        ///
-        /// Date.now().getTime(),
+use std::time::Duration;
 
-        use std::time::Duration;
-        use js_sys::Date;
-
-        pub struct ConnectionManager {
-            heartbeat_duration: f64,
-            timeout_duration: f64,
-            last_sent: f64,
-            last_heard: f64,
-        }
-
-        impl ConnectionManager {
-            pub fn new(heartbeat_duration: Duration, timeout_duration: Duration) -> Self {
-                ConnectionManager {
-                    last_sent: Date::now(),
-                    last_heard: Date::now(),
-                    heartbeat_duration: heartbeat_duration.as_millis() as f64,
-                    timeout_duration: timeout_duration.as_millis() as f64,
-                }
-            }
-
-            pub fn mark_sent(&mut self) {
-                self.last_sent = Date::now();
-            }
-
-            pub fn should_send_heartbeat(&self) -> bool {
-                (Date::now() - self.last_sent) > self.heartbeat_duration
-            }
-
-            pub fn mark_heard(&mut self) {
-                self.last_heard = Date::now();
-            }
-
-            pub fn should_drop(&self) -> bool {
-                (Date::now() - self.last_heard) > self.timeout_duration
-            }
-        }
-    }
-    else {
-        /// UDP ///
-        use std::time::{Duration, Instant};
-
-        pub struct ConnectionManager {
-            heartbeat_duration: Duration,
-            last_sent: Instant,
-            timeout_duration: Duration,
-            last_heard: Instant,
-        }
-
-        impl ConnectionManager {
-            pub fn new(heartbeat_duration: Duration, timeout_duration: Duration) -> Self {
-                ConnectionManager {
-                    last_sent: Instant::now(),
-                    heartbeat_duration,
-                    last_heard: Instant::now(),
-                    timeout_duration,
-                }
-            }
-
-            pub fn mark_sent(&mut self) {
-                self.last_sent = Instant::now();
-            }
-
-            pub fn should_send_heartbeat(&self) -> bool {
-                Instant::now().duration_since(self.last_sent) > self.heartbeat_duration
-            }
-
-            pub fn mark_heard(&mut self) {
-                self.last_heard = Instant::now();
-            }
-
-            pub fn should_drop(&self) -> bool {
-                Instant::now().duration_since(self.last_heard) > self.timeout_duration
-            }
-        }
-    }
+pub struct ConnectionManager {
+    heartbeat_timer: Timer,
+    timeout_timer: Timer,
 }
 
+impl ConnectionManager {
+    pub fn new(heartbeat_duration: Duration, timeout_duration: Duration) -> Self {
+        ConnectionManager {
+            heartbeat_timer: Timer::new(heartbeat_duration),
+            timeout_timer: Timer::new(timeout_duration),
+        }
+    }
 
+    pub fn mark_sent(&mut self) {
+        self.heartbeat_timer.reset();
+    }
 
+    pub fn should_send_heartbeat(&self) -> bool {
+        self.heartbeat_timer.ringing()
+    }
+
+    pub fn mark_heard(&mut self) {
+        self.timeout_timer.reset();
+    }
+
+    pub fn should_drop(&self) -> bool {
+        self.timeout_timer.ringing()
+    }
+}
---
+++ src/timer.rs
@@ -0,0 +1,58 @@
+
+cfg_if! {
+    if #[cfg(target_arch = "wasm32")] {
+        /// Wasm ///
+
+        use std::time::Duration;
+        use js_sys::Date;
+
+        pub struct Timer {
+            duration: f64,
+            last: f64,
+        }
+
+        impl Timer {
+            pub fn new(duration: Duration) -> Self {
+                Timer {
+                    last: Date::now(),
+                    duration: duration.as_millis() as f64,
+                }
+            }
+
+            pub fn reset(&mut self) {
+                self.last = Date::now();
+            }
+
+            pub fn ringing(&self) -> bool {
+                Instant::now().duration_since(self.last) > self.duration
+                (Date::now() - self.last) > self.duration
+            }
+        }
+    }
+    else {
+        /// Linux ///
+        use std::time::{Duration, Instant};
+
+        pub struct Timer {
+            duration: Duration,
+            last: Instant,
+        }
+
+        impl Timer {
+            pub fn new(duration: Duration) -> Self {
+                Timer {
+                    last: Instant::now(),
+                    duration,
+                }
+            }
+
+            pub fn reset(&mut self) {
+                self.last = Instant::now();
+            }
+
+            pub fn ringing(&self) -> bool {
+                Instant::now().duration_since(self.last) > self.duration
+            }
+        }
+    }
+}
