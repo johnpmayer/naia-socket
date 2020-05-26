dbb6236577cc269e1d035e28fbd7fd8bc277e3a9
naia-rs <naia.rust.crate@gmail.com>
2020-05-26 13:46:26 -0600

    went too far with individual config properties, coalescing them all into a "connectionless" flag

--- src/connection_manager.rs
+++ src/connection_manager.rs
@@ -5,31 +5,38 @@ use std::time::Duration;
 use crate::Timer;
 
 pub struct ConnectionManager {
-    heartbeat_timer: Timer,
-    timeout_timer: Timer,
+    heartbeat_timer: Option<Timer>,
+    timeout_timer: Option<Timer>,
 }
 
 impl ConnectionManager {
     pub fn new(heartbeat_duration: Duration, timeout_duration: Duration) -> Self {
         ConnectionManager {
-            heartbeat_timer: Timer::new(heartbeat_duration),
-            timeout_timer: Timer::new(timeout_duration),
+            heartbeat_timer: Some(Timer::new(heartbeat_duration)),
+            timeout_timer: Some(Timer::new(timeout_duration)),
+        }
+    }
+
+    pub fn connectionless() -> Self {
+        ConnectionManager {
+            heartbeat_timer: None,
+            timeout_timer: None,
         }
     }
 
     pub fn mark_sent(&mut self) {
-        self.heartbeat_timer.reset();
+        self.heartbeat_timer.as_mut().unwrap().reset();
     }
 
     pub fn should_send_heartbeat(&self) -> bool {
-        self.heartbeat_timer.ringing()
+        self.heartbeat_timer.as_ref().unwrap().ringing()
     }
 
     pub fn mark_heard(&mut self) {
-        self.timeout_timer.reset();
+        self.timeout_timer.as_mut().unwrap().reset();
     }
 
     pub fn should_drop(&self) -> bool {
-        self.timeout_timer.ringing()
+        self.timeout_timer.as_ref().unwrap().ringing()
     }
 }
