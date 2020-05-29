928c553f1a1d0697c89c56ffee10d4e8a01cbb2a
naia-rs <naia.rust.crate@gmail.com>
2020-05-29 15:49:11 -0600

    expose error & create empty packet easily

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,4 @@ pub use socket_event::SocketEvent;
 pub use message_sender::MessageSender;
 pub use gaia_socket_shared::{Config, find_my_ip_address};
 pub use packet::Packet;
+pub use error::GaiaClientSocketError;
