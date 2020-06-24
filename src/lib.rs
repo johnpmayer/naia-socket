d44a6e0452ee3180334641d9f6e098766d7f953a
naia-rs <naia.rust.crate@gmail.com>
2020-06-24 17:03:10 -0600

    connectionless is now required

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,11 @@ extern crate cfg_if;
 pub mod find_available_port;
 pub mod find_my_ip_address;
 
-mod constants;
-pub use constants::{MessageHeader, DEFAULT_MTU};
-
 mod config;
 pub use config::Config;
 
 mod utils;
 pub use utils::{StringUtils};
-
-mod connection_manager;
-pub use connection_manager::ConnectionManager;
 
 mod timer;
 pub use timer::Timer;
