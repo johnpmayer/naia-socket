8359a44127c0bef484cd5218345d911911e3e694
naia-rs <naia.rust.crate@gmail.com>
2020-06-28 16:10:51 -0600

    updating cargo

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,3 +1,10 @@
+#![deny(missing_docs,
+    missing_debug_implementations,
+    trivial_casts, trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces, unused_qualifications)]
+
 extern crate log;
 
 #[macro_use]
