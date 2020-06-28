e1378342517b8f106e7d8e2860cf5292ac09df4f
naia-rs <naia.rust.crate@gmail.com>
2020-06-27 18:34:43 -0600

    using rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-
 #[macro_use]
 extern crate cfg_if;
-
 extern crate log;
+
+use app::App;
 
 mod app;
 mod loop_native;
-
-use app::App;
 
 fn main() {
     // Uncomment the line below to enable logging. You don't need it if something else (e.g. quicksilver) is logging for you
