2c9b14d07ccf15fbe2609d0fef048c572a243277
naia-rs <naia.rust.crate@gmail.com>
2020-06-27 18:59:46 -0600

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-
 #[macro_use]
 extern crate log;
 
 use simple_logger;
 
-use naia_server_socket::{ServerSocket, SocketEvent, Config, Packet, find_my_ip_address};
+use naia_server_socket::{find_my_ip_address, Config, Packet, ServerSocket, SocketEvent};
 
 const SERVER_PORT: &str = "14191";
 const PING_MSG: &str = "ping";
@@ -12,7 +11,6 @@ const PONG_MSG: &str = "pong";
 
 #[tokio::main]
 async fn main() {
-
     simple_logger::init_with_level(log::Level::Info).expect("A logger was already initialized");
 
     info!("Naia Server Socket Example Started");
@@ -21,7 +19,8 @@ async fn main() {
 
     let config = Config::default();
 
-    let mut server_socket = ServerSocket::listen(current_socket_address.as_str(), Some(config)).await;
+    let mut server_socket =
+        ServerSocket::listen(current_socket_address.as_str(), Some(config)).await;
 
     let mut sender = server_socket.get_sender();
 
@@ -30,7 +29,6 @@ async fn main() {
             Ok(event) => {
                 match event {
                     SocketEvent::Packet(packet) => {
-
                         let address = packet.address();
                         let message = String::from_utf8_lossy(packet.payload());
                         info!("Server recv <- {}: {}", address, message);
@@ -38,8 +36,10 @@ async fn main() {
                         if message.eq(PING_MSG) {
                             let to_client_message: String = PONG_MSG.to_string();
                             info!("Server send -> {}: {}", address, to_client_message);
-                            sender.send(Packet::new(address, to_client_message.into_bytes()))
-                                .await.expect("send error");
+                            sender
+                                .send(Packet::new(address, to_client_message.into_bytes()))
+                                .await
+                                .expect("send error");
                         }
                     }
                     SocketEvent::Tick => {
