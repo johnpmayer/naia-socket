3d3c3b947bc4ac53a1cb68be5eef401afd6166f4
naia-rs <naia.rust.crate@gmail.com>
2020-06-27 18:09:21 -0600

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,19 @@
 
+#[macro_use]
+extern crate cfg_if;
+
 extern crate log;
 
 mod app;
 mod loop_native;
 
-use log::info;
-use naia_client_socket::{find_my_ip_address};
 use app::App;
-
-const SERVER_PORT: &str = "3179";
 
 fn main() {
     // Uncomment the line below to enable logging. You don't need it if something else (e.g. quicksilver) is logging for you
     simple_logger::init_with_level(log::Level::Info).expect("A logger was already initialized");
 
-    info!("Client Example Started");
-
-    let server_socket_address = find_my_ip_address::get() + ":" + SERVER_PORT;
-
-    let mut app = App::new(&server_socket_address);
+    let mut app = App::new();
 
     loop_native::start_loop(&mut app);
 }
