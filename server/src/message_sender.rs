7b3ec8ed072ec86161d4d4da8b1dd61b41c5e6b2
naia-rs <naia.rust.crate@gmail.com>
2020-07-02 11:46:59 -0600

    UDP Server Socket is now truly async

--- server/src/message_sender.rs
+++ server/src/message_sender.rs
@@ -2,76 +2,29 @@ use std::error::Error;
 
 use crate::Packet;
 
-cfg_if! {
-    if #[cfg(feature = "use-webrtc")] {
-        // WebRTC Message Sender
-        use futures_channel;
-        use futures_util::SinkExt;
+use futures_channel;
+use futures_util::SinkExt;
 
-        /// Handles sending messages to a Client that has established a connection with the Server socket
-        #[derive(Debug)]
-        pub struct MessageSender {
-            internal: futures_channel::mpsc::Sender<Packet>,
-        }
+/// Handles sending messages to a Client that has established a connection with
+/// the Server socket
+#[derive(Debug)]
+pub struct MessageSender {
+    internal: futures_channel::mpsc::Sender<Packet>,
+}
 
-        impl MessageSender {
-            /// Create a new MessageSender, given a reference to a async channel connected to the RtcServer
-            pub fn new(sender: futures_channel::mpsc::Sender<Packet>) -> MessageSender {
-                MessageSender {
-                    internal: sender,
-                }
-            }
-            /// Send a Packet to a client
-            pub async fn send(&mut self, packet: Packet) -> Result<(), Box<dyn Error + Send>> {
-                match self.internal.send(packet).await {
-                    Ok(content) => { Ok(content) },
-                    Err(error) => { return Err(Box::new(error)); }
-                }
-            }
-        }
+impl MessageSender {
+    /// Create a new MessageSender, given a reference to a async channel
+    /// connected to the RtcServer
+    pub fn new(sender: futures_channel::mpsc::Sender<Packet>) -> MessageSender {
+        MessageSender { internal: sender }
     }
-    else if #[cfg(feature = "use-udp")] {
-        // UDP Message Sender
-        use tokio::net::UdpSocket;
 
-        use std::{
-            rc::Rc,
-            cell::RefCell,
-            net::SocketAddr,
-            collections::HashSet,
-        };
-
-        /// Handles sending messages to a Client that has established a connection with the Server socket
-        #[derive(Clone, Debug)]
-        pub struct MessageSender {
-            socket: Rc<RefCell<UdpSocket>>,
-            clients: Rc<RefCell<HashSet<SocketAddr>>>,
-        }
-
-        impl MessageSender {
-            /// Create a new MessageSender, if supplied with the UdpSocket and a reference to a list
-            /// of established clients
-            pub fn new(socket: Rc<RefCell<UdpSocket>>, clients: Rc<RefCell<HashSet<SocketAddr>>>) -> MessageSender {
-                MessageSender {
-                    socket,
-                    clients,
-                }
-            }
-
-            /// Send a Packet to a client
-            pub async fn send(&mut self, packet: Packet) -> Result<(), Box<dyn Error + Send>> {
-                let address = packet.address();
-                if !self.clients.borrow_mut().contains(&address) {
-                    panic!("sending to an unknown client?");
-                }
-
-                //send it
-                if let Err(err) = self.socket.as_ref().borrow_mut().send_to(&packet.payload(), address).await {
-                    return Err(Box::new(err));
-                }
-                else {
-                    return Ok(());
-                }
+    /// Send a Packet to a client
+    pub async fn send(&mut self, packet: Packet) -> Result<(), Box<dyn Error + Send>> {
+        match self.internal.send(packet).await {
+            Ok(content) => Ok(content),
+            Err(error) => {
+                return Err(Box::new(error));
             }
         }
     }
--- server/src/udp_server_socket/mod.rs
+++ server/src/udp_server_socket/mod.rs
@@ -1,85 +1,118 @@
-use std::{cell::RefCell, collections::HashSet, io::ErrorKind, net::SocketAddr, rc::Rc};
+use futures_channel::mpsc;
+use futures_util::{pin_mut, select, FutureExt, StreamExt};
+use std::{io::Error as IoError, net::SocketAddr};
+use tokio::{
+    net::UdpSocket,
+    time::{self, Interval},
+};
 
-use tokio::net::UdpSocket;
-
-use naia_socket_shared::{Config, Timer};
+use naia_socket_shared::Config;
 
 use crate::{error::NaiaServerSocketError, Packet};
 
 use super::{message_sender::MessageSender, socket_event::SocketEvent};
 
+const CLIENT_CHANNEL_SIZE: usize = 8;
+
 #[derive(Debug)]
 pub struct UdpServerSocket {
-    socket: Rc<RefCell<UdpSocket>>,
+    socket: UdpSocket,
+    to_client_sender: mpsc::Sender<Packet>,
+    to_client_receiver: mpsc::Receiver<Packet>,
+    tick_timer: Interval,
     receive_buffer: Vec<u8>,
-    message_sender: MessageSender,
-    tick_timer: Timer,
-    clients: Rc<RefCell<HashSet<SocketAddr>>>,
 }
 
 impl UdpServerSocket {
     pub async fn listen(socket_address: SocketAddr, config: Option<Config>) -> UdpServerSocket {
-        let socket = Rc::new(RefCell::new(UdpSocket::bind(socket_address).await.unwrap()));
+        let socket = UdpSocket::bind(socket_address).await.unwrap();
 
         let tick_interval = match config {
             Some(config) => config.tick_interval,
             None => Config::default().tick_interval,
         };
 
-        let clients = Rc::new(RefCell::new(HashSet::new()));
-        let message_sender = MessageSender::new(socket.clone(), clients.clone());
+        let (to_client_sender, to_client_receiver) = mpsc::channel(CLIENT_CHANNEL_SIZE);
 
         UdpServerSocket {
             socket,
-            receive_buffer: vec![0; 1472], //should be input from config
-            message_sender,
-            tick_timer: Timer::new(tick_interval),
-            clients,
+            to_client_sender,
+            to_client_receiver,
+            tick_timer: time::interval(tick_interval),
+            receive_buffer: vec![0; 0x10000], /* Hopefully get rid of this one day.. next version
+                                               * of webrtc-unreliable should make that happen */
         }
     }
 
     pub async fn receive(&mut self) -> Result<SocketEvent, NaiaServerSocketError> {
-        let mut output: Option<Result<SocketEvent, NaiaServerSocketError>> = None;
-        while output.is_none() {
-            if self.tick_timer.ringing() {
-                self.tick_timer.reset();
-                output = Some(Ok(SocketEvent::Tick));
-                continue;
-            }
+        enum Next {
+            FromClientMessage(Result<(usize, SocketAddr), IoError>),
+            ToClientMessage(Packet),
+            PeriodicTimer,
+        }
 
-            let buffer: &mut [u8] = self.receive_buffer.as_mut();
-            match self
-                .socket
-                .borrow_mut()
-                .recv_from(buffer)
-                .await
-                .map(move |(recv_len, address)| (&buffer[..recv_len], address))
-            {
-                Ok((payload, address)) => {
-                    if !self.clients.borrow().contains(&address) {
-                        self.clients.borrow_mut().insert(address);
+        loop {
+            let next = {
+                let timer_next = self.tick_timer.tick().fuse();
+                pin_mut!(timer_next);
+
+                let to_client_receiver_next = self.to_client_receiver.next().fuse();
+                pin_mut!(to_client_receiver_next);
+
+                let receive_buffer = &mut self.receive_buffer;
+                let udp_socket = &mut self.socket;
+                let from_client_message_receiver_next = udp_socket.recv_from(receive_buffer).fuse();
+                pin_mut!(from_client_message_receiver_next);
+
+                select! {
+                    from_client_result = from_client_message_receiver_next => {
+                        Next::FromClientMessage(from_client_result)
                     }
-                    let packet = Packet::new(address, payload.to_vec());
-                    output = Some(Ok(SocketEvent::Packet(packet)));
-                    continue;
+                    to_client_message = to_client_receiver_next => {
+                        Next::ToClientMessage(
+                            to_client_message.expect("to server message receiver closed")
+                        )
+                    }
+                    _ = timer_next => {
+                        Next::PeriodicTimer
+                    }
                 }
-                Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
-                    //just didn't receive anything this time
+            };
+
+            match next {
+                Next::FromClientMessage(from_client_message) => match from_client_message {
+                    Ok((message_len, message_address)) => {
+                        let payload: Vec<u8> = self.receive_buffer[0..message_len]
+                            .iter()
+                            .cloned()
+                            .collect();
+                        return Ok(SocketEvent::Packet(Packet::new_raw(
+                            message_address,
+                            payload.into_boxed_slice(),
+                        )));
+                    }
+                    Err(err) => {
+                        return Err(NaiaServerSocketError::Wrapped(Box::new(err)));
+                    }
+                },
+                Next::ToClientMessage(packet) => {
+                    let address = packet.address();
+
+                    match self.socket.send_to(packet.payload(), &address).await {
+                        Err(_) => {
+                            return Err(NaiaServerSocketError::SendError(address));
+                        }
+                        _ => {}
+                    }
                 }
-                Err(err) => {
-                    output = Some(Err(NaiaServerSocketError::Wrapped(Box::new(err))));
-                    continue;
+                Next::PeriodicTimer => {
+                    return Ok(SocketEvent::Tick);
                 }
             }
         }
-        return output.unwrap();
     }
 
     pub fn get_sender(&mut self) -> MessageSender {
-        return self.message_sender.clone();
-    }
-
-    pub fn get_clients(&mut self) -> Vec<SocketAddr> {
-        self.clients.borrow().iter().cloned().collect()
+        return MessageSender::new(self.to_client_sender.clone());
     }
 }
