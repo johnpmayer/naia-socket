818c1f3a95ca4a767c63f906da7320a4020439b6
naia-rs <naia.rust.crate@gmail.com>
2020-07-06 11:45:23 -0600

    - find_my_ip_address() docs comment is *slightly* better

--- shared/src/find_my_ip_address.rs
+++ shared/src/find_my_ip_address.rs
@@ -2,7 +2,7 @@ use std::{
     net::{IpAddr, Ipv4Addr, Ipv6Addr},
 };
 
-/// Given an IPv4 Address, attempt to find an available port on the current host
+/// Helper method to find local IP address, if possible
 pub fn find_my_ip_address() -> Option<IpAddr> {
     let ip = local_ipaddress::get().unwrap_or_default();
 
