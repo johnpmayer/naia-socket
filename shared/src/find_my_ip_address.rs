0fe4f86c143ef7fd2fb5a4741f6ddd71e507e528
Slyklaw Dev <slyklaw@gmail.com>
2020-07-06 08:49:43 +0700

    Remove Linux-specific ip detection.

--- shared/src/find_my_ip_address.rs
+++ shared/src/find_my_ip_address.rs
@@ -1,42 +1,16 @@
 use std::{
     net::{IpAddr, Ipv4Addr, Ipv6Addr},
-    process::Command,
 };
 
 /// Given an IPv4 Address, attempt to find an available port on the current host
 pub fn find_my_ip_address() -> Option<IpAddr> {
-    let output = match Command::new("hostname").args(&["-I"]).output() {
-        Ok(ok) => ok,
-        Err(_) => {
-            return None;
-        }
-    };
+    let ip = local_ipaddress::get().unwrap_or_default();
 
-    let stdout = match String::from_utf8(output.stdout) {
-        Ok(ok) => ok,
-        Err(_) => {
-            return None;
-        }
-    };
-
-    let ips: Vec<&str> = stdout.trim().split(" ").collect::<Vec<&str>>();
-    let first = ips.first();
-    match first {
-        Some(first) => {
-            if !first.is_empty() {
-                if let Ok(addr) = first.parse::<Ipv4Addr>() {
-                    return Some(IpAddr::V4(addr));
-                } else if let Ok(addr) = first.parse::<Ipv6Addr>() {
-                    return Some(IpAddr::V6(addr));
-                } else {
-                    return None;
-                }
-            } else {
-                return None;
-            }
-        }
-        None => {
-            return None;
-        }
+    if let Ok(addr) = ip.parse::<Ipv4Addr>() {
+        return Some(IpAddr::V4(addr));
+    } else if let Ok(addr) = ip.parse::<Ipv6Addr>() {
+        return Some(IpAddr::V6(addr));
+    } else {
+        return None;
     }
 }
