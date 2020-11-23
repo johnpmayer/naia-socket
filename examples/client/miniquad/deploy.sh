25fb46ad2191a1f6e9b6cbd57b2e78dcd5ad6464
Connor Carpenter <naia.rust.crate@gmail.com>
2020-11-23 13:42:25 -0700

    miniquad deploy script working correctly in new project structure

--- examples/client/miniquad/deploy.sh
+++ examples/client/miniquad/deploy.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
-xdg-open http://localhost:3179/
+xdg-open http://localhost:3177/
 
 # replace 'client' & 'webserver' below with the appropriate directory names for your project
-client='naia-interop-demo'
+working_dir='examples/client/miniquad'
+client='naia-client-socket-miniquad-example'
 webserver_dir='dev-http-server'
 
 get_reload_actions(){
   local OUTPUT=''
-  local c=$1
-  local w=$2
-  FMT='rm -rfv %s/dist/** &&
-  cp -a static/. %s/dist/ &&
+  local d=$1
+  local c=$2
+  local w=$3
+  FMT='rm -rf %s/%s/dist &&
+  mkdir %s/%s/dist &&
   cargo build --target wasm32-unknown-unknown --bin %s &&
-  cp target/wasm32-unknown-unknown/debug/%s.wasm %s/dist/%s.wasm &&
-  cd %s &&
+  cp target/wasm32-unknown-unknown/debug/%s.wasm %s/%s/dist/%s.wasm &&
+  cp -a %s/static/. %s/%s/dist/ &&
+  cd %s/%s &&
   cargo run --bin %s'
-  printf -v OUTPUT "$FMT" $w $w $c $c $w $c $w $w
+  printf -v OUTPUT "$FMT" $d $w $d $w $c $c $d $w $c $d $d $w $d $w $w
   echo $OUTPUT
 }
 
-actions="$(get_reload_actions $client $webserver_dir)"
+actions="$(get_reload_actions $working_dir $client $webserver_dir)"
 watchexec -r -s SIGKILL --ignore $webserver_dir/dist --ignore target --clear "$actions"
-
-#FMT='rm -rfv %s/dist/** &&
-#  cp -a static/. %s/dist/ &&
-#  cargo build --target wasm32-unknown-unknown --bin %s &&
-#  cp target/wasm32-unknown-unknown/debug/%s.wasm %s/dist/%s.wasm &&
-#  $ (cd %s && cargo run --bin %s)'
