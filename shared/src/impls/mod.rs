f64afd8049879c709d703db2bf9c3b469e65a898
Connor Carpenter <naia.rust.crate@gmail.com>
2020-11-28 15:23:10 -0700

    - bringing in some structs from naia that require JS interop (trying to do all JS interop in this crate, rather than in naia)

--- shared/src/impls/mod.rs
+++ shared/src/impls/mod.rs
@@ -4,17 +4,20 @@ cfg_if! {
         pub use self::wasm_bindgen::timer::Timer;
         pub use self::wasm_bindgen::random::Random;
         pub use self::wasm_bindgen::instant::Instant;
+        pub use self::wasm_bindgen::timestamp::Timestamp;
     }
     else if #[cfg(all(target_arch = "wasm32", feature = "mquad"))] {
         mod miniquad;
         pub use self::miniquad::random::Random;
         pub use self::miniquad::timer::Timer;
         pub use self::miniquad::instant::Instant;
+        pub use self::miniquad::timestamp::Timestamp;
     }
     else {
         mod native;
         pub use native::random::Random;
         pub use native::timer::Timer;
         pub use native::instant::Instant;
+        pub use native::timestamp::Timestamp;
     }
 }
